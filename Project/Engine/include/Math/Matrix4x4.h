b25ceecbc4fd942a1ce7ea4b7e45776c280e76b4
sTsenre <h.yonghengernest@digipen.edu>
2025-09-03 14:13:51 +0800

    Matrix File created

---
+++ Project/Engine/include/Math/Matrix4x4.h
@@ -0,0 +1,14 @@
+/*********************************************************************************
+* @File			Matrix4x4.h
+* @Author		Ernest Ho, h.yonghengernest@digipen.edu
+* @Co-Author	-
+* @Date			3/9/2025
+* @Brief		This is the Declaration of 4x4 Matrix Class
+*
+* Copyright (C) 20xx DigiPen Institute of Technology. Reproduction or disclosure
+* of this file or its contents without the prior written consent of DigiPen
+* Institute of Technology is prohibited.
+*********************************************************************************/
+#pragma once
+
+#include "pch.h"
---
+++ Project/Engine/src/Math/Matrix4x4.cpp
@@ -0,0 +1,13 @@
+/*********************************************************************************
+* @File			Matrix4x4.cpp
+* @Author		Ernest Ho, h.yonghengernest@digipen.edu
+* @Co-Author	-
+* @Date			3/9/2025
+* @Brief		This is the Defination of 4x4 Matrix Class
+*
+* Copyright (C) 20xx DigiPen Institute of Technology. Reproduction or disclosure
+* of this file or its contents without the prior written consent of DigiPen
+* Institute of Technology is prohibited.
+*********************************************************************************/
+
+#include "Math/Matrix4x4.h"
