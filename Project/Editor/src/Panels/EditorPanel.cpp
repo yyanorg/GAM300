6bce468be8da9ca2ead8b4ea31936d7ea6da02b9
Zik <112854745+DigiZik@users.noreply.github.com>
2025-09-09 01:47:55 +0800

    Save layout to imgui.ini
    Update EditorPanel.cpp

--- Project/Editor/src/Panels/EditorPanel.cpp
+++ Project/Editor/src/Panels/EditorPanel.cpp
@@ -1,4 +1,4 @@
-#include "../include/Panels/EditorPanel.hpp"
+#include "Panels/EditorPanel.hpp"
 
 EditorPanel::EditorPanel(const std::string& panelName, bool isOpenByDefault)
     : m_Name(panelName), m_IsOpen(isOpenByDefault) {
