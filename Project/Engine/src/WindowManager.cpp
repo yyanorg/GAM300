6d157f9533fbed216491a49c150faf5b97191ccf
austinang35 <aajj_austinang.gmail.com>
2025-09-09 11:50:06 +0800

    cap 60fps

--- Project/Engine/src/WindowManager.cpp
+++ Project/Engine/src/WindowManager.cpp
@@ -199,11 +199,36 @@ bool WindowManager::IsWindowFocused() {
     return isFocused;
 }
 
+//void WindowManager::updateDeltaTime() {
+//    double currentFrameTime = glfwGetTime();
+//    deltaTime = currentFrameTime - lastFrameTime;
+//    lastFrameTime = currentFrameTime;
+//
+//    //glfwMakeContextCurrent(ptrWindow);
+//    //glfwSwapInterval(1);    //enable Vsync (only limited to your own monitor)
+//}
+
 void WindowManager::updateDeltaTime() {
-    double currentFrameTime = glfwGetTime();
-    deltaTime               = currentFrameTime - lastFrameTime;
-    lastFrameTime           = currentFrameTime;
+    const double targetDeltaTime = 1.0 / 60.0; // cap at 60fps
+
+    double currentTime = glfwGetTime();
+    double frameTime = currentTime - lastFrameTime;
+
+    double remainingTime = targetDeltaTime - frameTime;
+
+    // Sleep only if we have at least 5 ms remaining
+    if (remainingTime > 0.005) {
+        std::this_thread::sleep_for(std::chrono::milliseconds((int)((remainingTime - 0.001) * 1000)));
+    }
+    // Busy-wait the last few milliseconds
+    while ((glfwGetTime() - lastFrameTime) < targetDeltaTime) {}
+
+    // Update deltaTime
+    currentTime = glfwGetTime();
+    deltaTime = currentTime - lastFrameTime;
+    lastFrameTime = currentTime;
 }
+
 double WindowManager::getDeltaTime() {
     return deltaTime;
 }
